21f758c1a7fb04d7b45d3e9bdfc8911c33fa5ae2
Disperindag ESDM Kalbar <disperindagesdm.kalbarprov@gmail.com>
2025-01-02 15:35:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -32,6 +32,14 @@ function validatePassword() {
     }
 }
 
+function showHide() {
+  var inputan = document.getElementById("passwordKu");
+  if (inputan.type === "password") {
+    inputan.type = "text";
+  } else {
+    inputan.type = "password";
+  }
+} 
 
 function openModal() {
     document.getElementById("passwordModal").style.display = "flex";
